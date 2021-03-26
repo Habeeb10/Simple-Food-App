1407ce08075700a6a83e5cb950a77f12439c4924
habeeb10 <79145763+habeeb10@users.noreply.github.com>
2021-03-26 23:28:56 +0100

    main components

---
+++ sreens/order.js
@@ -0,0 +1,157 @@
+import React from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+const OrderList = [
+  {
+    foodName: "Cheese Burger",
+    toppings: "Beef, Veggies & Chilli",
+    price: "2000",
+    foodImage: require("../chees.png"),
+    delImage: require("../delete.png"),
+  },
+  {
+    foodName: "Large Pizza",
+    toppings: "Extra Cheese & Toppings",
+    price: "2070",
+    foodImage: require("../pizza.png"),
+    delImage: require("../delete.png"),
+  },
+  {
+    foodName: "Grilled Turkey",
+    toppings: "Sauce and Pepper",
+    price: "3500",
+    foodImage: require("../turkey.png"),
+    delImage: require("../delete.png"),
+  },
+  {
+    foodName: "Peppersoup",
+    toppings: "Beef & Mutton",
+    price: "4000",
+    foodImage: require("../soup.png"),
+    delImage: require("../delete.png"),
+  },
+];
+
+const ItemCard = ({ foodImage, foodName, toppings, price, delImage }) => {
+  return (
+    <View style={styles.itemCont}>
+      <View style={styles.leftBox}>
+        <Image style={styles.img} source={foodImage} />
+        <View>
+          <Text style={styles.cheese}>{foodName} </Text>
+          <Text>{toppings} </Text>
+        </View>
+      </View>
+      <View style={styles.rightBox}>
+        <View>
+          <Text>{`NGN ${price}`} </Text>
+        </View>
+        <Image style={styles.img} source={delImage} />
+      </View>
+    </View>
+  );
+};
+
+export default Order = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.ord}>My Orders</Text>
+      <Text style={styles.cart}>
+        Welcome to your cart, preview items below.
+      </Text>
+
+      {OrderList.map((item) => (
+        <ItemCard
+          foodImage={item.foodImage}
+          foodName={item.foodName}
+          toppings={item.toppings}
+          price={item.price}
+          delImage={item.delImage}
+        />
+      ))}
+      <View style={styles.total}>
+        <Text style={styles.tot}>Total:</Text>
+        <Text style={styles.money}>{`NGN ${20000}`}</Text>
+      </View>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => navigation.navigate("login")}
+        style={styles.log}
+      >
+        <Text style={styles.login}>CHECKOUT</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  money: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  tot: {
+    marginLeft: 10,
+  },
+  log: {
+    // marginTop: 30,
+    backgroundColor: "#E25F38",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+    height: 50,
+    width: "100%",
+    alignSelf: "center",
+  },
+  login: {
+    color: "white",
+  },
+  total: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 30,
+  },
+  cheese: {
+    fontWeight: "bold",
+  },
+  img: {
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  leftBox: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rightBox: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  itemCont: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  container: {
+    marginTop: 40,
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  ord: {
+    fontWeight: "bold",
+    marginTop: 100,
+    fontSize: 40,
+  },
+  cart: {
+    marginTop: 5,
+    marginBottom: 30,
+  },
+});
---
+++ sreens/signup.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { CustomInput } from "../component/CustomInput";
+
+export default function Signup({ navigation }) {
+  const [modalState, setModalState] = useState(false);
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+      <Text style={styles.welcome}>Create your Account</Text>
+      <Text style={styles.account}>it's free and easy to setup!</Text>
+      <Text style={styles.use}>Username</Text>
+      <CustomInput placeHolder="Richard" />
+      <Text style={styles.email}>Email</Text>
+      <CustomInput />
+      <Text style={styles.pass}>Password</Text>
+      <CustomInput />
+      <TouchableOpacity
+        style={styles.signup}
+        onPress={() => setModalState(true)}
+      >
+        <Text style={styles.login}>SIGNUP</Text>
+      </TouchableOpacity>
+      <View style={styles.new}>
+        <Text style={styles.user}>Existing user?</Text>
+        <TouchableOpacity
+          style={styles.log}
+          activeOpacity={0.7}
+          onPress={() => navigation.navigate("login")}
+        >
+          <Text style={styles.up}>Signin</Text>
+        </TouchableOpacity>
+      </View>
+      {modalState && (
+        <View style={styles.modalContainer}>
+          <View style={styles.modalBox}>
+            <View style={styles.top}></View>
+            <Image
+              source={require("../myicon.png")}
+              resizeMode="contain"
+              style={styles.image}
+            />
+            <Text style={styles.default}>Default Wallet Selection</Text>
+            <Text style={styles.proceed}>
+              If you proceed, All cards transactions will be charged from the
+              selected wallet.
+            </Text>
+            <TouchableOpacity
+              style={styles.conf}
+              onPress={() => navigation.navigate("select")}
+            >
+              <Text style={styles.confirm}>CONFIRM</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.here}
+              onPress={() => setModalState(false)}
+            >
+              <Text style={styles.click}>Click here to cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: "center",
+  },
+  top: {
+    backgroundColor: "#E0E6ED",
+    width: "10%",
+    height: 5,
+    alignSelf: "center",
+    marginTop: 10,
+    borderRadius: 3,
+  },
+  here: {
+    alignSelf: "center",
+    marginTop: 30,
+  },
+  click: {
+    color: "#E25F38",
+  },
+  conf: {
+    backgroundColor: "#FDF3F1",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 60,
+    height: 50,
+    width: "90%",
+    alignSelf: "center",
+  },
+  confirm: {
+    fontSize: 15,
+    color: "#E25F38",
+    fontWeight: "bold",
+  },
+  proceed: {
+    color: "#9DA8B6",
+    alignSelf: "center",
+    width: "70%",
+    textAlign: "center",
+    marginTop: 15,
+  },
+  default: {
+    marginTop: 20,
+    alignSelf: "center",
+    fontSize: 25,
+    fontWeight: "500",
+  },
+  modalBox: {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: "white",
+    height: 479,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  modalContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: "rgba(0,0,0,.4)",
+  },
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    paddingHorizontal: 30,
+  },
+  welcome: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 240,
+  },
+  account: {
+    fontSize: 15,
+  },
+  email: {
+    fontSize: 15,
+    marginTop: 30,
+  },
+  use: {
+    fontSize: 15,
+    marginTop: 60,
+  },
+  pass: {
+    fontSize: 15,
+    marginTop: 30,
+  },
+
+  signup: {
+    marginTop: 40,
+    backgroundColor: "#E25F38",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+    height: 50,
+    width: "100%",
+    alignSelf: "center",
+  },
+  login: {
+    color: "white",
+  },
+  new: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: 30,
+  },
+  up: {
+    color: "#E25F38",
+  },
+});
